7a65ed458913cfa63ca6263249f629b616ba4b16
RBalezantis <33401073+RBalezantis@users.noreply.github.com>
2025-08-11 20:55:36 -0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // Change base depending on type of site:
 
 export default defineConfig({
-  base: '/KalymnosClimbingGuide/', // for project site
+  base: /KalymnosClimbingGuide/, // for project site
   // base: '/', // for username.github.io
   plugins: [react()],
 })
