139f7831cc20f5e8585a3262626917ec1aaecde9
RBalezantis <33401073+RBalezantis@users.noreply.github.com>
2025-08-11 21:27:30 -0700

    Update CragHeading.jsx

--- src/components/CragHeading.jsx
+++ src/components/CragHeading.jsx
@@ -2,16 +2,16 @@ import React from "react";
 import "./styles/med-theme.css";
 
 const artMap = {
-  "Grande Grotta": "/artwork/grande-grotta.svg",
-  "Sikati Cave": "/artwork/sikati-cave.svg",
-  "Odyssey": "/artwork/odyssey.svg",
-  "Motor City": "/artwork/motor-city.svg",
-  "Vlychadia": "/artwork/vlychadia.svg",
-  "Telendos - Princess Canyon": "/artwork/telendos-princess-canyon.svg",
-  "St Constantine": "/artwork/st-constantine.svg",
-  "Arginonta Valley": "/artwork/arginonta-valley.svg",
-  "Kasteli / Panormos": "/artwork/kasteli-panormos.svg",
-  "Panorama": "/artwork/panorama.svg"
+  "Grande Grotta": "artwork/grande-grotta.svg",
+  "Sikati Cave": "artwork/sikati-cave.svg",
+  "Odyssey": "artwork/odyssey.svg",
+  "Motor City": "artwork/motor-city.svg",
+  "Vlychadia": "artwork/vlychadia.svg",
+  "Telendos - Princess Canyon": "artwork/telendos-princess-canyon.svg",
+  "St Constantine": "artwork/st-constantine.svg",
+  "Arginonta Valley": "artwork/arginonta-valley.svg",
+  "Kasteli / Panormos": "artwork/kasteli-panormos.svg",
+  "Panorama": "artwork/panorama.svg"
 };
 
 export default function CragHeading({ cragKey, subtitle }) {
