d5be5f02a6e3f7016b0254b891a38eeeedc20d72
RBalezantis <33401073+RBalezantis@users.noreply.github.com>
2025-08-11 21:31:22 -0700

    Update CragHeading.jsx

--- src/components/CragHeading.jsx
+++ src/components/CragHeading.jsx
@@ -2,16 +2,16 @@ import React from "react";
 import "./styles/med-theme.css";
 
 const artMap = {
-  "Grande Grotta": "artwork/grande-grotta.svg",
-  "Sikati Cave": "artwork/sikati-cave.svg",
-  "Odyssey": "artwork/odyssey.svg",
-  "Motor City": "artwork/motor-city.svg",
-  "Vlychadia": "artwork/vlychadia.svg",
-  "Telendos - Princess Canyon": "artwork/telendos-princess-canyon.svg",
-  "St Constantine": "artwork/st-constantine.svg",
-  "Arginonta Valley": "artwork/arginonta-valley.svg",
-  "Kasteli / Panormos": "artwork/kasteli-panormos.svg",
-  "Panorama": "artwork/panorama.svg"
+  "Grande Grotta": `${import.meta.env.BASE_URL}artwork/grande-grotta.svg`,
+  "Sikati Cave": `${import.meta.env.BASE_URL}artwork/sikati-cave.svg`,
+  "Odyssey": `${import.meta.env.BASE_URL}artwork/odyssey.svg`,
+  "Motor City": `${import.meta.env.BASE_URL}artwork/motor-city.svg`,
+  "Vlychadia": `${import.meta.env.BASE_URL}vlychadia.svg`,
+  "Telendos - Princess Canyon": `${import.meta.env.BASE_URL}artwork/telendos-princess-canyon.svg`,
+  "St Constantine": `${import.meta.env.BASE_URL}artwork/st-constantine.svg`,
+  "Arginonta Valley": `${import.meta.env.BASE_URL}artwork/arginonta-valley.svg`,
+  "Kasteli / Panormos": `${import.meta.env.BASE_URL}artwork/kasteli-panormos.svg`,
+  "Panorama": `${import.meta.env.BASE_URL}artwork/panorama.svg`
 };
 
 export default function CragHeading({ cragKey, subtitle }) {
