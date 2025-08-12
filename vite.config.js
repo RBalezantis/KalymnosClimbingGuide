4cde7e2ca78a10df26516b91b74fdd597a492c29
RBalezantis <rbalezantis@shaw.ca>
2025-08-11 19:58:41 -0700

    update

--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,10 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-export default defineConfig({plugins:[react()],server:{port:5173}});
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// Change base depending on type of site:
+
+export default defineConfig({
+  base: '/KalymnosClimbingGuide/', // for project site
+  // base: '/', // for username.github.io
+  plugins: [react()],
+})
